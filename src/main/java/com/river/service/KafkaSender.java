e5494892a75b5d7609db6f5df2407c21fd5dc2c9
Giang Tran Truong <ttgiang@tma.com.vn>
2020-05-27 14:14:08 +0700

    SpringBoot ApacheKafka

---
+++ src/main/java/com/river/service/KafkaSender.java
@@ -0,0 +1,18 @@
+package com.river.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaSender {
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	
+	String kafkaTopic = "river_topic";
+	
+	public void send(String message) {
+	    kafkaTemplate.send(kafkaTopic, message);
+	}
+}
